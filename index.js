97a783acc1cb51f016a0848b25d5831c2e4e1b9f
Lucio Schraier <lucio.ese@gmail.com>
2025-01-19 20:56:32 +0000

    feat: url stores into a variable

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+var counter = 1;
+const shortenedUrl = {};
+
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post("/api/shorturl", (req, res) => {
+  const url = req.body.url;
+  console.log(url);
+  shortenedUrl[counter] = url;
+  counter += 1;
+  console.log(shortenedUrl);
+})
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
